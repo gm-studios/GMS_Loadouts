a734c7c47bdaf2d7510996f70e470f1d3820d3bd
Gordon Macfarlane <109997768+macfargc@users.noreply.github.com>
2025-06-07 00:18:21 +0100

    New file: fxmanifest.lua
    Includes the following scripts: client.lua, server.lua and config.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- GMStudios Simple Loadout system for standalone servers.
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GMStudios'
+description 'Loadout system for standalone servers'
+version '1.0.0'
+name 'GMS_Loadouts'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
