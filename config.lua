0a320a0918aa2224bd7baa0ba8331bb89658ed4f
Gordon Macfarlane <109997768+macfargc@users.noreply.github.com>
2025-06-07 00:16:02 +0100

    New file: config.lua
    Added in a config.lua with a default loadout.

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Loadouts = {
+  ["default"] = {
+    discordRoleId: 1380683324217098310,
+    weapons: {
+      "weapon_stungun",
+      "wepaon_nightstick",
+      "weapon_speedcuffs",
+      "weapon_pepperspray"
+    }
+  }
+}
